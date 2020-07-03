f530e15c67cfab49381b60abb631d8fe867c30f1
Aryaman Kakad <askvit2000@gmail.com>
2020-07-03 19:55:52 +0530

    Modify Equal Tuple

---
+++ Modify_EqualTuples.py
@@ -0,0 +1,15 @@
+tuple_data = [[(6, 10), (3, 16)], [(2, 20), (13, 9)], [(7, 16), (4, 18)]] 
+print("The original list is : " + str(tuple_data)) 
+ 
+test_tuple = [(2, 20), (13, 9)] 
+print("The test row is: " + str(test_tuple))
+
+for i, j in enumerate(tuple_data): 
+	if j == test_tuple: 
+		output = [] 
+		for k in j: 
+			out = (k[0] * 3, k[1] * 4) 
+			output.append(out) 
+		tuple_data[i] = output 
+ 
+print("List after modification : " + str(tuple_data)) 
