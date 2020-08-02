852b69660cfefa1cb4c606f868247fd1427ee0f1
Aryaman Kakad <askvit2000@gmail.com>
2020-08-02 21:07:01 +0530

    Webscapping Lxml

--- WebScraping_LXML.py
+++ WebScraping_LXML.py
@@ -1,34 +1,20 @@
 import requests
 import lxml.html
 
-html = requests.get('https://store.steampowered.com/explore/new/')
-doc = lxml.html.fromstring(html.content)
+website = requests.get('https://store.steampowered.com/explore/new/')
+document = lxml.html.fromstring(website.content)
 
-new_releases = doc.xpath('//div[@id="tab_newreleases_content"]')[0]
+releases_new = document.xpath('//div[@id="tab_newreleases_content"]')[0]
 
-titles = new_releases.xpath('.//div[@class="tab_item_name"]/text()')
-prices = new_releases.xpath('.//div[@class="discount_final_price"]/text()')
+game_titles = releases_new.xpath('.//div[@class="tab_item_name"]/text()')
+game_prices = releases_new.xpath('.//div[@class="discount_final_price"]/text()')
 
-tags = [tag.text_content() for tag in new_releases.xpath('.//div[@class="tab_item_top_tags"]')]
-tags = [tag.split(', ') for tag in tags]
 
-platforms_div = new_releases.xpath('.//div[@class="tab_item_details"]')
-total_platforms = []
+result = []
+for information in zip(game_titles,game_prices):
+    out = {}
+    out['game_titles'] = information[0]
+    out['game_prices'] = information[1]
+    result.append(out)
 
-for game in platforms_div:
-    temp = game.xpath('.//span[contains(@class, "platform_img")]')
-    platforms = [t.get('class').split(' ')[-1] for t in temp]
-    if 'hmd_separator' in platforms:
-        platforms.remove('hmd_separator')
-    total_platforms.append(platforms)
-
-output = []
-for info in zip(titles,prices, tags, total_platforms):
-    resp = {}
-    resp['title'] = info[0]
-    resp['price'] = info[1]
-    resp['tags'] = info[2]
-    resp['platforms'] = info[3]
-    output.append(resp)
-
-print(output)
+print(result[0:3])
